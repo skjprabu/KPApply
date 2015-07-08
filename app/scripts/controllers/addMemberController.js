7eb8508c0f28bcd6d97ca3733c6b819f140db8e3
skjprabu <jayaprabhu.kandasamy@perficient.com>
2015-07-08 13:38:00 -0600

    reset memberData after success

--- app/scripts/controllers/addMemberController.js
+++ app/scripts/controllers/addMemberController.js
@@ -11,14 +11,15 @@ angular.module('kpapply')
     .controller('AddMemberController', ['$scope', '$log','$http', function ($scope, $log, $http) {
         $scope.memberData = {'gender': 'Male', 'previous_customer': 'Yes'};
         var logger = $log.getInstance('addMemberCOntroller');
-        logger.info("alpha");
+
 
 
         $scope.addMember = function (memberData){
-            logger.info("Parameter  "+memberData);
+
             console.log("adding member", $scope.memberData );
             console.log("adding member", memberData);
             var parameter = JSON.stringify(memberData);
+            logger.info("Parameter  "+parameter);
             $http.post("/writeData",parameter).
                 success(function(data, status, headers, config){
                     $scope.memberData = {'gender': 'Male', 'previous_customer': 'Yes'};
