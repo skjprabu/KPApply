23ee6748e5f3544f7bc37850aa7dce1f679a6a4e
aboorde <andrew.boorde@gmail.com>
2015-07-08 11:49:15 -0600

    Added Calendar Controls

---
+++ app/scripts/controllers/CalendarController.js
@@ -0,0 +1,23 @@
+/**
+ * Created by andrew.boorde on 7/8/2015.
+ */
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularApp.controller:CalendarCtrl
+ * @description
+ *
+ * Calendar controller
+ */
+angular.module('kpapply')
+    .controller('CalendarController', function ($scope) {
+        $scope.open = function($event) {
+            $event.preventDefault();
+            $event.stopPropagation();
+
+            $scope.opened = true;
+        };
+        $scope.format = 'yyyy/MM/dd';
+        $scope.maxDate = $scope.maxDate ? null : new Date();
+    });
