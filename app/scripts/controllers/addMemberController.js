531c130d7bc0a2cb0b1e6d81ffaa7a9743c53b5c
skjprabu <jayaprabhu.kandasamy@perficient.com>
2015-07-08 14:00:52 -0400

    SubmitForm Included
    -Updated server.js for post function
    - Updated addMemberController.js for form submit

--- app/scripts/controllers/addMemberController.js
+++ app/scripts/controllers/addMemberController.js
@@ -8,11 +8,17 @@
  * Add Member controller
  */
 angular.module('kpapply')
-    .controller('AddMemberController', ['$scope', function ($scope) {
+    .controller('AddMemberController', ['$scope', '$log','$http', function ($scope, $log, $http) {
         $scope.memberData = {'gender': 'Male', 'previous_customer': 'Yes'};
+        var logger = $log.getInstance('MainController');
+        logger.info("Checking message");
 
-
-        $scope.addMember = function (){
-            console.log("adding member");
+        $scope.addMember = function (memberData){
+            console.log("adding member", memberData);
+            var parameter = JSON.stringify(memberData);
+            $http.post("/writeData",parameter);
         };
+
+
+
     }]);
